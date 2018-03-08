1e6ca91446dc6794b2e51a2b89023401c42d3500
Deta Teguh Satrio <detatsatrio@outlook.com>
2018-03-09 00:12:23 +0700

    add contact and some features

--- mail.php
+++ mail.php
@@ -3,11 +3,8 @@ $con_name = $_POST['con_name'];
 $con_email = $_POST['con_email'];
 $con_message = $_POST['con_message'];
 
-
-
-
-$to = 'onurolker@gmail.com';   
-$subject = 'Stephans Form';
+$to = 'detatsatrio@outlook.com';
+$subject = 'Proposal Project';
 
 $message = '<strong>Name : </strong>'.$con_name.'<br/><br/>';
 
@@ -18,7 +15,7 @@ $headers = 'From: '.$con_name.' '.$con_email . "\r\n" ;
 $headers .='Reply-To: '. $to . "\r\n" ;
 $headers .='X-Mailer: PHP/' . phpversion();
 $headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
+$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
 
 mail($to,$subject,$message,$headers);
 echo 1;
